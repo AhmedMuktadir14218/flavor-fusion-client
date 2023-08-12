90b807772eaacbf3ce4206b75cbbf28376ff4d80
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-08-12 20:15:56 +0600

    cover

--- .history/src/Pages/Shared/Cover/Cover_20230812190022.jsx
+++ .history/src/Pages/Shared/Cover/Cover_20230812190022.jsx
@@ -8,7 +8,7 @@ const Cover = ({ img, title }) => {
             bgImageAlt="the menu"
             strength={-200}
         >
-            <div className="hero h-[600px]">
+            <div className="hero h-[00px]">
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
