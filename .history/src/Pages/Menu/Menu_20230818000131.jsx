69db7d5901aff1460b482631e41ba4d88405511e
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-08-18 00:02:01 +0600

    MenuCategory

---
+++ .history/src/Pages/Menu/Menu_20230818000131.jsx
@@ -0,0 +1,40 @@
+import { Helmet } from "react-helmet-async";
+import Cover from "../Shared/Cover/Cover";
+
+import useMenu from "../../Hooks/useMenu";
+import SectionTitle from "../../Components/SectionTitle";
+import MenuCategory from "./MenuCategory/MenuCategory";
+import menuImg from "../../assets/menu/menu-bg.png";
+import dessertImg from "../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../assets/menu/pizza-bg.jpg";
+import saladImg from "../../assets/menu/salad-bg.jpg";
+import soupImg from "../../assets/menu/soup-bg.jpg";
+// import PopularMenu from "../Home/PopularMenu/PopularMenu";
+
+const Menu = () => {
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
+    return (
+        <div>
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+            </Helmet>
+            <Cover img={menuImg} title="our menu"></Cover>
+            {/* main cover */}
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+            {/* offered menu items */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* dessert menu items  */}
+            <MenuCategory items={desserts} title="dessert" img={dessertImg}></MenuCategory>
+            <MenuCategory items={pizza} title={"pizza"} img={pizzaImg}></MenuCategory>
+            <MenuCategory items={salad} title={"salad"} img={saladImg}></MenuCategory>
+            <MenuCategory items={soup} title={"soup"} img={soupImg}></MenuCategory>
+        </div>
+    );
+};
+
+export default Menu;
