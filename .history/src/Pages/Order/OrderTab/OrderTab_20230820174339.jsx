07c6413f116e3213dbd94967becee77e7140e86f
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-08-20 17:44:09 +0600

    order

---
+++ .history/src/Pages/Order/OrderTab/OrderTab_20230820174339.jsx
@@ -0,0 +1,34 @@
+
+const OrderTab = ({ items }) => {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    };
+    return (
+        <div >
+
+            <Swiper
+                pagination={pagination}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className='grid md:grid-cols-3 gap-10'>
+                        {
+                            items.map(item => <FoodCard
+                                key={item._id}
+                                item={item}
+                            ></FoodCard>)
+                        }
+                    </div>
+
+                </SwiperSlide>
+
+            </Swiper>
+        </div>
+    );  npm i swiper
+};
+
+export default OrderTab;
