1d1af84a244e77765cc5156a7512c16467b08c59
AhmedMuktadirUdoy <100287308+AhmedMuktadir14218@users.noreply.github.com>
2023-09-08 23:31:01 +0600

    Update PopularMenu.jsx

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -6,8 +6,7 @@ import useMenu from "../../../Hooks/useMenu";
 const PopularMenu = () => {
   const [menu]=useMenu();
   const popular =menu.filter(item => item.category === 'popular')
-    // useEffect(() => {
-    //     fetch('menu.json')
+    
     //         .then(res => res.json())
     //         .then(data => {
     //             const popularItems = data.filter(item => item.category === 'popular');
