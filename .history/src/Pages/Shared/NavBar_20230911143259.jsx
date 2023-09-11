a1850b4cef37851f83e501e0b377c858f9251056
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-09-11 14:35:51 +0600

    sds

--- .history/src/Pages/Shared/NavBar_20230911143259.jsx
+++ .history/src/Pages/Shared/NavBar_20230911143259.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/menu'>Menu</Link></li>
         <li><Link to='/order/salad'>Order</Link></li>
-        {/* <li><Link to='/order'>Order</Link></li> */}
+        <li><Link to='/order'>Order</Link></li>
     </>
 
     return (
