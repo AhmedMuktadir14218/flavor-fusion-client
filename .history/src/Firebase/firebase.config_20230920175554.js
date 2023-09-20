ce3fc7a35426c14e3d318acbdc846ad48dba2b4c
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-09-20 17:57:03 +0600

    firebase

---
+++ .history/src/Firebase/firebase.config_20230920175554.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAz9NSkoWce61lui2G0IbE1idpOR1QaIj0",
+  authDomain: "fusion-flavour.firebaseapp.com",
+  projectId: "fusion-flavour",
+  storageBucket: "fusion-flavour.appspot.com",
+  messagingSenderId: "864073146204",
+  appId: "1:864073146204:web:0bf3cea96c684775c6eb99"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export  default app;
